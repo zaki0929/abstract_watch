3f71f1bb5b8881fa14d3991a15ac315180f28c05
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2019-01-07 11:38:11 +0900

    Clean up main.js

--- main.js
+++ main.js
@@ -50,25 +50,25 @@ window.addEventListener("load", function(){
             strokeWidth: r*0.8 
         });
 
-	unit.turnON = function () {
-           this.setFill(onColor);
-	}
+        unit.turnON = function () {
+            this.setFill(onColor);
+        }
 
-	unit.turnOFF = function () {
-           this.setFill(offColor);
-	}
+        unit.turnOFF = function () {
+            this.setFill(offColor);
+        }
 
-	return unit;
+        return unit;
     }
 
     AW.buildUnitsBar = function (group, array, num, x, y, r, d, color) {
         for (var i=0; i<num; i++) {
             var unit = AW.buildUnit(x+(d*i), y, r, color);
-	    if (typeof group != 'undefined') {
-	        group.add(unit);
+            if (typeof group != 'undefined') {
+                group.add(unit);
             }
-	    if (typeof array != 'undefined') {
-	        array.push(unit);
+            if (typeof array != 'undefined') {
+                array.push(unit);
             }
         }
     }
@@ -82,13 +82,12 @@ window.addEventListener("load", function(){
 
     AW.update = function() {
         var now  = new Date()
-
-	AW.updateUnits(now.getMonth(),   AW.monthArray, false);
-	AW.updateUnits(now.getDate(),    AW.dateArray, true);
-	AW.updateUnits(now.getDay(),     AW.dayArray, false);
-	AW.updateUnits(now.getHours(),   AW.hourArray, true);
-	AW.updateUnits(now.getMinutes(), AW.minArray, true);
-	AW.updateUnits(now.getSeconds(), AW.secArray, true);
+        AW.updateUnits(now.getMonth(),   AW.monthArray, false);
+        AW.updateUnits(now.getDate()-1,  AW.dateArray,  false);
+        AW.updateUnits(now.getDay(),     AW.dayArray,   false);
+        AW.updateUnits(now.getHours(),   AW.hourArray,  true);
+        AW.updateUnits(now.getMinutes(), AW.minArray,   true);
+        AW.updateUnits(now.getSeconds(), AW.secArray,   true);
         AW.unitLayer.draw();
     }
 
@@ -99,10 +98,8 @@ window.addEventListener("load", function(){
             } else {
                 array[i].turnOFF();
             }
-	}
+        }
     }
 
     setInterval(AW.update, 1000);
-
-
 });
