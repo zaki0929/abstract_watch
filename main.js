49e916e62eb166aa20073f2c08acfd799078e233
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2019-02-11 16:34:40 +0900

    Add animation

--- main.js
+++ main.js
@@ -16,20 +16,6 @@ window.addEventListener("load", function(){
     AW.unitLayer = new Konva.Layer();
     AW.stage.add(AW.unitLayer);
 
-    AW.monthGroup = new Konva.Group();
-    AW.dateGroup  = new Konva.Group();
-    AW.dayGroup   = new Konva.Group();
-    AW.hourGroup  = new Konva.Group();
-    AW.minGroup   = new Konva.Group();
-    AW.secGroup   = new Konva.Group();
-
-    AW.unitLayer.add(AW.monthGroup);
-    AW.unitLayer.add(AW.dateGroup);
-    AW.unitLayer.add(AW.dayGroup);
-    AW.unitLayer.add(AW.hourGroup);
-    AW.unitLayer.add(AW.minGroup);
-    AW.unitLayer.add(AW.secGroup);
-
     AW.monthArray = [];
     AW.dateArray  = [];
     AW.dayArray   = [];
@@ -37,57 +23,67 @@ window.addEventListener("load", function(){
     AW.minArray   = [];
     AW.secArray   = [];
 
-    AW.buildUnit = function(x, y, r, color) {
+    AW.buildUnit = function(array, layer, x, y, r, color) {
         var onColor  = color;
         var offColor = AW.offColor;
 
-        var unit = new Konva.Circle({
+        var base = new Konva.Circle({
             x: x,
             y: y,
             radius: r,
             fill: offColor,
-            stroke: offColor,
-            strokeWidth: r*0.8 
+            strokeWidth: 0 
+        });
+	layer.add(base);
+
+        var unit = new Konva.Circle({
+            x: x,
+            y: y,
+            radius: 0,
+            fill: onColor,
+            strokeWidth: 0 
+        });
+	layer.add(unit);
+
+        var tween = new Konva.Tween({
+            node: unit,
+            duration: 0.5,
+            fill : onColor,
+            radius: r*0.5 
         });
 
-        unit.turnON = function () {
-            this.setFill(onColor);
-        }
+	unit.turnON = function () {
+	    tween.play();
+	}
 
-        unit.turnOFF = function () {
-            this.setFill(offColor);
-        }
-
-        return unit;
+	unit.turnOFF = function () {
+	    tween.reset();
+	}
+	array.push(unit);
     }
 
-    AW.buildUnitsBar = function (group, array, num, x, y, r, d, color) {
+    AW.buildUnitsBar = function (array, layer, num, x, y, r, d, color) {
         for (var i=0; i<num; i++) {
-            var unit = AW.buildUnit(x+(d*i), y, r, color);
-            if (typeof group != 'undefined') {
-                group.add(unit);
-            }
-            if (typeof array != 'undefined') {
-                array.push(unit);
-            }
+            AW.buildUnit(array, layer, x+(d*i), y, r, color);
         }
     }
 
-    AW.buildUnitsBar(AW.monthGroup, AW.monthArray, 12, AW.width/(59+3)*2, 100, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
-    AW.buildUnitsBar(AW.dateGroup,  AW.dateArray,  31, AW.width/(59+3)*2, 150, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
-    AW.buildUnitsBar(AW.dayGroup,   AW.dayArray,   7,  AW.width/(59+3)*2, 200, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
-    AW.buildUnitsBar(AW.hourGroup,  AW.hourArray,  23, AW.width/(59+3)*2, 250, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
-    AW.buildUnitsBar(AW.minGroup,   AW.minArray,   59, AW.width/(59+3)*2, 300, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
-    AW.buildUnitsBar(AW.secGroup,   AW.secArray,   59, AW.width/(59+3)*2, 350, AW.width/(59+3)*0.3, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.monthArray, AW.unitLayer, 12, AW.width/(59+3)*2, 100, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.dateArray,  AW.unitLayer, 31, AW.width/(59+3)*2, 150, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.dayArray,   AW.unitLayer, 7,  AW.width/(59+3)*2, 200, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.hourArray,  AW.unitLayer, 23, AW.width/(59+3)*2, 250, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.minArray,   AW.unitLayer, 59, AW.width/(59+3)*2, 300, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
+    AW.buildUnitsBar(AW.secArray,   AW.unitLayer, 59, AW.width/(59+3)*2, 350, AW.width/(59+3)*0.4, AW.width/(59+3), '#F0BC08');
 
     AW.update = function() {
         var now  = new Date()
-        AW.updateUnits(now.getMonth(),   AW.monthArray, false);
-        AW.updateUnits(now.getDate()-1,  AW.dateArray,  false);
-        AW.updateUnits(now.getDay(),     AW.dayArray,   false);
-        AW.updateUnits(now.getHours(),   AW.hourArray,  true);
-        AW.updateUnits(now.getMinutes(), AW.minArray,   true);
-        AW.updateUnits(now.getSeconds(), AW.secArray,   true);
+
+	AW.updateUnits(now.getMonth(),   AW.monthArray, false);
+	AW.updateUnits(now.getDate(),    AW.dateArray, true);
+	AW.updateUnits(now.getDay(),     AW.dayArray, false);
+	AW.updateUnits(now.getHours(),   AW.hourArray, true);
+	AW.updateUnits(now.getMinutes(), AW.minArray, true);
+	AW.updateUnits(now.getSeconds(), AW.secArray, true);
         AW.unitLayer.draw();
     }
 
@@ -98,7 +94,7 @@ window.addEventListener("load", function(){
             } else {
                 array[i].turnOFF();
             }
-        }
+	}
     }
 
     setInterval(AW.update, 1000);
